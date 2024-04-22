15ba44e6cdd6dd518f6a84bacd5524db5fb40f75
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-04-22 13:05:02 +0300

    Setting up routing and basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TodoComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
