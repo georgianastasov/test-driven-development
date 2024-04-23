ea27529dd90aa9847b42d5613403457f1c0eaf2a
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-04-23 10:41:40 +0300

    feat: Initializing ToDo list component with empty array

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IgxListModule } from 'igniteui-angular';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, IgxListModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
-export class TodoComponent {}
+export class TodoComponent {
+  todoList: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todoList = [];
+  }
+}
